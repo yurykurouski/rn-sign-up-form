7bc126865b5c6329176efbeb19c9ab8a65db5cc0
Yury Kurouski <yury_kurouski@epam.com>
2024-02-29 22:03:12 +0100

    feat: text constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,31 @@
 export const COMPANY_SITE = 'https://valetax.com/';
+
+//region
+export const SIGN_UP_REGION_LABEL = 'Region';
+export const SIGN_UP_REGION_PLACEHOLDER = '- Choose your region - ';
+export const SIGN_UP_REGION_SEARCH_PLACEHOLDER = 'Type region name';
+
+//mail
+export const SIGN_UP_EMAIL_LABEL = 'Email';
+export const SIGN_UP_EMAIL_PLACEHOLDER = 'Enter Your Email';
+
+//pwd
+export const SIGN_UP_PASSWORD_LABEL = 'Password';
+export const SIGN_UP_PASSWORD_PLACEHOLDER = 'Create a Password';
+
+//repeat pwd
+export const SIGN_UP_REPEAT_PASSWORD_LABEL = 'Repeat Password';
+export const SIGN_UP_REPEAT_PASSWORD_PLACEHOLDER = 'Repeat Password';
+
+//repeat pwd
+export const SIGN_UP_PARTNER_CODE_LABEL = 'Partner Code (optional)';
+export const SIGN_UP_PARTNER_CODE_PLACEHOLDER = 'Partner Code';
+
+export const SIGNUP_SCREEN_TITLE = 'Sign Up';
+
+export const CONFIRM_AGREEMENT =
+  "I confirm that I'm not a U.S. person for tax purposes and agree with the ";
+export const CLIENT_AGREEMENT = 'Client Agreement';
+
+export const ALREADY_HAVE = 'Already have an account? ';
+export const LOGIN = 'Login';
