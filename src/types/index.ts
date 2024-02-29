f140e425752c902cb148f190833e108706d6cde1
Yury Kurouski <yury_kurouski@epam.com>
2024-02-29 20:28:38 +0100

    chore: types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,65 @@
+import { Control, FieldError } from 'react-hook-form';
+import { TextInputProps, ViewStyle } from 'react-native';
+
 export enum EInputType {
   PASSWORD = 'password',
   EMAIL = 'email',
+}
+
+export enum EFormInputName {
+  REGION = 'region',
+  EMAIL = 'email',
+  PASSWORD = 'password',
+  REPEAT_PWD = 'repeatPassword',
+  PARTNER_CODE = 'partnerCode',
+}
+
+export type TFormInputs = {
+  [EFormInputName.REGION]: string;
+  [EFormInputName.EMAIL]: string;
+  [EFormInputName.PASSWORD]: string;
+  [EFormInputName.REPEAT_PWD]: string;
+  [EFormInputName.PARTNER_CODE]?: string;
+};
+
+export type TFormInputProps = {
+  control: Control<TFormInputs, any, TFormInputs>;
+  type: EInputType;
+  formName: EFormInputName;
+  inputName: string;
+  inputPlaceholder: string;
+  trigger?: () => Promise<boolean>;
+  error?: FieldError | string[];
+  topMargin?: boolean;
+  optional?: boolean;
+};
+
+export type TPwdHintProps = {
+  hint: {
+    type: string[];
+    message: string;
+  };
+  isDirty: boolean;
+  errors?: string[];
+};
+
+export type TInputProps = TextInputProps & {
+  value?: string;
+  onChangeText: (text: string) => void;
+  name: string;
+  type?: EInputType;
+  placeholder?: string;
+  isNewPwd?: boolean;
+  optional?: boolean;
+  containerStyle?: ViewStyle;
+};
+
+export enum ERR_TYPES {
+  MATCHES = 'matches',
+  REQUIRED = 'required',
+  MIN = 'min',
+  MIN_NUMBER = 'minNumber',
+  MIN_UPPERCASE = 'minUppercase',
+  MIN_LOWERCASE = 'minLowercase',
+  MIN_SYMBOL = 'minSymbol',
 }
