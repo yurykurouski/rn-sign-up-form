0ad8219304dce9620e83f66ed7f2760e02e32555
Yury Kurouski <yury_kurouski@epam.com>
2024-02-29 20:25:30 +0100

    feat: add filterErrs util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,14 @@
 import { Platform } from 'react-native';
+import { ERR_TYPES } from 'types';
+import { FieldError } from 'react-hook-form';
 
 export const isIOS = Platform.OS === 'ios';
 export const isAndroid = Platform.OS === 'android';
+
+export const filterErrs = (whitelist: ERR_TYPES[], fieldErr?: FieldError) => {
+  if (fieldErr?.types) {
+    return Object.keys(fieldErr?.types).filter(err =>
+      whitelist.includes(err as ERR_TYPES),
+    );
+  }
+};
