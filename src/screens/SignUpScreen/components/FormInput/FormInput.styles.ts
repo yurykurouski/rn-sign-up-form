2e8fe964d6481c11b800b1069f9038d808e1ecc3
Yury Kurouski <yury_kurouski@epam.com>
2024-03-01 14:21:04 +0100

    chore: remove err styles

--- src/screens/SignUpScreen/components/FormInput/FormInput.styles.ts
+++ src/screens/SignUpScreen/components/FormInput/FormInput.styles.ts
@@ -1,14 +1,7 @@
-import { RED } from 'assets';
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   margin20: {
     marginTop: 20,
-  },
-  errMsg: {
-    marginTop: 6,
-    color: RED,
-    fontFamily: 'Inter',
-    fontWeight: '500',
   },
 });
